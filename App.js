bce24e6a01c9b4756ed2afa8694bd1afa190cb2d
hovanhoa <hoa.ho.van@hcmut.edu.vn>
2022-04-01 00:05:08 +0700

    Gas Screen

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import Profile from "./screens/ProfileScreen/index.js";
+import Gas from "./screens/GasScreen/index.js";
 
 export default function App() {
-  return <Profile />;
+  return <Gas />;
 }
